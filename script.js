aa57f78f2987be13a402fa470b8e95c4c19706b3
borosmihai39 <borosmihai39@gmail.com>
2021-08-09 15:54:05 +0300

    progress

--- script.js
+++ script.js
@@ -1,50 +1,36 @@
-const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
 const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
-const textBox = document.getElementById('text-box');
+var textBox = document.getElementsByClassName('text-box');
+
 
 // Dark Mode Styles
 function darkMode(){
     
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    // can use replace here
-    // toggleIcon.children[1].classList.remove('fa-sun');
-    
-    image1.src = 'img/undraw_proud_coder_dark.svg';
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)'; 
+for(var i = 0; i < textBox.length; i++) {
+    textBox[i].style.backgroundColor = 'rgb(255 255 255 / 50%)';
+}
+    toggleIcon.children[0].textContent = 'Dark Mode';   
+    image1.src = 'img/undraw_Goals_dark.svg';
     image2.src = 'img/undraw_solution_mindset_dark.svg';
-    image3.src = 'img/undraw_conceptual_idea_dark.svg';
+    image3.src = 'img/undraw_work_together_dark.svg';
 }
 
 // Light Mode Styles
 function lightMode(){
     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    toggleIcon.children[0].textContent = 'Light Mode';
-    // toggleIcon.children[1].classList.remove('fa-moon');
-    // toggleIcon.children[1].classList.add('fa-sun');
-    image1.src = 'img/undraw_proud_coder_light.svg';
+    for(var i = 0; i < textBox.length; i++) {
+        textBox[i].style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    }
+    toggleIcon.children[0].textContent = 'Light Mode';  
+    image1.src = 'img/undraw_Goals_light.svg';
     image2.src = 'img/undraw_solution_mindset_light.svg';
-    image3.src = 'img/undraw_conceptual_idea_light.svg';
+    image3.src = 'img/undraw_work_together_light.svg';
 }
-
-// Switch Theme Dynamically
-// function switchTheme(event){
-//     if(toggleIcon.children[1].classList.contains('fa-sun')){
-        
-//     } else {
-        
-//     }
-// }
-
-
-
-// Event Listener
-// toggleSwitch.addEventListener('change',switchTheme);
 
 function testswitch(){
     if (toggleIcon.children[1].classList.contains('fa-sun')){
@@ -62,7 +48,7 @@ function testswitch(){
 
 }
 
-toggleIcon.addEventListener('click',testswitch);
+window.onload = toggleIcon.addEventListener('click',testswitch);
 
 
 // Check local storage for theme
